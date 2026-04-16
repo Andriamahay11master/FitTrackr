16384985047d57c4fcc50b64c5e3d4bbafa67962
Mahay <hirimanana@yahoo.fr>
2026-04-16 21:59:18 +0400

    update WeightChart component

--- src/components/charts/WeightChart.tsx
+++ src/components/charts/WeightChart.tsx
@@ -1,5 +1,31 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { date: "Mon", weight: 102 },
+  { date: "Tue", weight: 101.5 },
+  { date: "Wed", weight: 101 },
+  { date: "Thu", weight: 100.5 },
+  { date: "Fri", weight: 100 },
+];
+
 const WeightChart = () => {
-  return <div>WeightChart</div>;
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}>
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="weight" strokeWidth={3} />
+      </LineChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default WeightChart;
