7aa17d1cff67ee53dc387f0e72e41ae5fa8d2473
Mahay <hirimanana@yahoo.fr>
2026-04-17 18:34:20 +0400

    add weighform on dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,14 @@
 import WeightCard from "../../components/ui/WeightCard";
 import ProgressCard from "../../components/ui/ProgressCard";
 import WeightChart from "../../components/charts/WeightChart";
+import { useWeight } from "../../hooks/useWeight";
+import AddWeightForm from "../../components/ui/AddWeightForm";
 
 const Dashboard = () => {
+  const { weights, addWeight } = useWeight();
+
+  const latestWeight =
+    weights.length > 0 ? weights[weights.length - 1].value : "--";
   return (
     <div className="p-6 space-y-6">
       {/* Header */}
@@ -11,9 +17,12 @@ const Dashboard = () => {
         <p className="text-gray-500">Track your progress</p>
       </div>
 
+      {/* Add Weight */}
+      <AddWeightForm onAdd={addWeight} />
+
       {/* Cards */}
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <WeightCard title="Current Weight" value="80 kg" />
+        <WeightCard title="Current Weight" value={latestWeight + " kg"} />
         <ProgressCard />
         <WeightCard title="Goal Weight" value="85 kg" />
       </div>
