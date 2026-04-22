b21c30046db574df674b9919a281b7cf879ad0d2
Mahay <hirimanana@yahoo.fr>
2026-04-22 16:32:42 +0400

    add protected Route

---
+++ src/components/routes/ProtectedRoutes.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { auth } from "../../services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (u) => {
+      if (!u) setUser(u);
+      setLoading(false);
+    });
+
+    return () => unsub();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  return user ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
