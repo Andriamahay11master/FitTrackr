91e8cbd88a2db0d1846bf6367497af498f72a372
Mahay <hirimanana@yahoo.fr>
2026-04-23 13:25:25 +0400

    fixed sidebar

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,9 +1,9 @@
 import { logout } from "../../services/authService";
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex min-h-screen bg-gray-100 relative">
       {/* Sidebar */}
-      <aside className="w-64 bg-white shadow-md p-5">
+      <aside className="fixed top-0 left-0 h-full w-64 bg-white shadow-md p-5 z-20">
         <h2 className="text-xl font-bold mb-6">FitTrack</h2>
 
         <nav className="space-y-3">
@@ -19,9 +19,9 @@ const AppLayout = ({ children }: { children: React.ReactNode }) => {
         </nav>
       </aside>
 
-      <div className="flex-1 flex flex-col">
+      <div className="flex flex-col flex-1 ml-64 mt-12">
         {/* Top bar */}
-        <div className="flex justify-end items-center p-4 bg-white border-b">
+        <div className="fixed inset-x-0 top-0 flex justify-end items-center p-4 bg-white border-b shadow-sm z-10">
           <button
             onClick={logout}
             className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600"
